c83240d85e582e07eb213e6280e5ce5116858597
Michael Roussel <pokemon_pidgeot@msn.com>
2019-08-11 11:52:41 -0400

    removed /build from gitignore

---
+++ client/build/precache-manifest.e2a19bbccc43917e2c743d5df367c5ef.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9e168025174e638aac1ca813aaa5ef7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8fa64e4cdf9116d8ac5",
+    "url": "/static/js/2.ffef699f.chunk.js"
+  },
+  {
+    "revision": "377b1fc15de3bbc72b18",
+    "url": "/static/js/main.8b2d1aad.chunk.js"
+  },
+  {
+    "revision": "aac10b1cbb2aec953c81",
+    "url": "/static/js/runtime~main.bdc68d36.js"
+  }
+]);
